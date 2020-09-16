563fb9e4540fb29559a72b24c0a04aa591413508
Da Risk <da_risk@geekorum.com>
2020-09-16 19:42:48 -0400

    Initial import

---
+++ dynamicfeature/src/main/java/com/geekorum/bug/dynamicfeature/FeatureActivity.kt
@@ -0,0 +1,12 @@
+package com.geekorum.bug.dynamicfeature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FeatureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feature)
+    }
+}
